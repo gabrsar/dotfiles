ac1f4870b3e3d28be4a89ff7f93e3ebd07aaed2e
Gabriel Saraiva <gabriel.saraiva@99taxis.com>
2017-08-11 17:05:29 -0300

    Vim improvements

--- vim/installVundle.sh
+++ vim/installVundle.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+ln -s $(pwd)/.vim $HOME/.vim
+ln -s $(pwd)/.vimrc $HOME/.vimrc
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Now open Vim and run: ':PluginInstall' command"
 
 
