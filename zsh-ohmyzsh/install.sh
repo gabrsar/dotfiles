07138cf1e8a28f896c26b8b18ac847124dcc67c2
Gabriel Saraiva <gabriel@yellow.app>
2018-06-14 14:20:05 -0300

    Add ZSH highlighting

--- zsh-ohmyzsh/install.sh
+++ zsh-ohmyzsh/install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo "Cloning..."
 
+echo "ZSH - Suggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-echo "Configuring..."
 sed 's/^plugins=(/plugins=(\n\tzsh-autosuggestions/' -i ~/.zshrc
+
+echo "ZSH - Highlight"
+highlighting=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-highlighting 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$highlighting"
+echo "source $highlight/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc 
+
 
 clear
 
